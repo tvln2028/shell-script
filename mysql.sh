3553e78fea5fbd5a79cf8f8700f33b7eb811d9e9
tvln2028.inform@gmail.com <tvln2028.inform@gmail.com>
2024-07-02 15:58:58 -0400

    changes

--- mysql.sh
+++ mysql.sh
@@ -10,14 +10,13 @@ Y="\e[33m"
 N="\e[0m"
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
+   if [ $1 -ne 0 ]
+   then
+         echo -e "$2...$R FAILURE $N"
+         exit 1
     else
-        echo -e "$2...$G SUCCESS $N"
+         echo -e "$2...$G SUCCESS $N"
     fi
-
 }
 
 if [$USERID -ne 0]
