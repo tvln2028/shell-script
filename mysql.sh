3eacad5666192e408a8803128dc9c7fa1fcb2f1d
tvln2028.inform@gmail.com <tvln2028.inform@gmail.com>
2024-07-04 16:07:36 -0400

    changes

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -37,5 +39,15 @@ VALIDATE $? "Enabling MYSQL Server"
 systemct1 start mysqld &>>$LOGFILE
 VALIDATE $? "Starting MYSQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#VALIDATE $? "Setting up root password"
+
+#below code will be useful for idempotent nature
+mysql -h db.daws78.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL root password setup"
+else
+    echo -e "mysql Root password is already setup...$Y SKIPPING $N"
+fi
