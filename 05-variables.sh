8a40c249cbf23c9ff43fc6843ab75da46dcb478a
tvln2028 <lakshminarayana4560@gmail.com>
2025-01-28 14:43:58 -0500

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,10 @@
 
 echo "Please enter your username::" #text entered here will be added as value to the ccreated variable
 
-read USERNAME # Here UERNAME is the variable name
+read -s USERNAME # Here UERNAME is the variable name
 
 echo "username entered: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD
