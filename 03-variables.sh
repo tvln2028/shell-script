7c99c053a34318f4a1982e19e86073fd4454a0da
tvln2028 <lakshminarayana4560@gmail.com>
2025-01-28 14:16:12 -0500

    coversation

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Robert
+PERSON2=Raj
+
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you?"
+echo "$PERSON2::I am good, how are you?"
