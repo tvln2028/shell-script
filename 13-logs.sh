5a15b34f3bf9c98ffcb3d6e1474f227aa24f7333
tvln2028 <lakshminarayana4560@gmail.com>
2025-01-29 16:26:32 -0500

    colors

---
+++ 13-logs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs" # created a folder shellscript-logs in cd /var/log in server by using mkdir shellscript-logs
+LOG_FILE=$(echo $0 | cut -d "." -f1 ) # f1 is fragment1 in log_file
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S) # date format for year-month-day-hour-minute-second
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing MySQL"
+else
+    echo -e "MySQL is already ... $Y INSTALLED $N"
+fi
+
+
+dnf list installed git &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing Git"
+else
+    echo -e "Git is already ... $Y INSTALLED $N"
+fi
