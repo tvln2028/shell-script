9cdeeae5b6ba623cc7e22867799afa00ba3ba54b
tvln2028 <lakshminarayana4560@gmail.com>
2025-01-30 06:29:08 -0500

    backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -9,7 +9,7 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} # if user is not providing number of days, we are taking 14 as default
 
-LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOGS_FOLDER="/home/ec2-user/shell-script-logs"
 LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
